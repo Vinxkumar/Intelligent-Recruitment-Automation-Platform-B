1c899424523cac522f9499258dd029fb1d320e10
Vinxkumar <vinxkumar178@gmail.com>
2026-07-10 16:06:12 +0530

    Initialized Backed for Intelligent-Recruitment-Automation-Platform

---
+++ src/main/java/com/example/RecruitmentAutomationPlatform/RecruitmentAutomationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecruitmentAutomationPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAutomationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAutomationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RecruitmentAutomationPlatform/RecruitmentAutomationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecruitmentAutomationPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAutomationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

288117927de40393781bf792f97501fdcba2ae6d
Vinxkumar <vinxkumar178@gmail.com>
2026-07-23 15:58:46 +0530

    New Exception for Jd Generation

---
+++ src/main/java/com/example/RecruitmentAutomationPlatform/exceptions/JdGenerateException.java
@@ -0,0 +1,7 @@
+package com.example.RecruitmentAutomationPlatform.exceptions;
+
+public class JdGenerateException extends RuntimeException {
+    public JdGenerateException(String message) {
+        super(message);
+    }
+}

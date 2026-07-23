a5ad478cf462bda14f7707b7fb3cd4fee7d8f61e
Vinxkumar <vinxkumar178@gmail.com>
2026-07-23 16:00:11 +0530

    wired jd generation function

--- src/main/java/com/example/RecruitmentAutomationPlatform/services/ServiceImpl/JobServiceImpl.java
+++ src/main/java/com/example/RecruitmentAutomationPlatform/services/ServiceImpl/JobServiceImpl.java
@@ -1,38 +1,66 @@
 package com.example.RecruitmentAutomationPlatform.services.ServiceImpl;
 
 import com.example.RecruitmentAutomationPlatform.config.ModelMapperConfig;
+import com.example.RecruitmentAutomationPlatform.dto.JobDto.JdRequest;
+import com.example.RecruitmentAutomationPlatform.dto.JobDto.JdResponse;
 import com.example.RecruitmentAutomationPlatform.dto.JobDto.JobDto;
 import com.example.RecruitmentAutomationPlatform.dto.JobDto.JobResponseDto;
 import com.example.RecruitmentAutomationPlatform.entity.JobEntity;
 import com.example.RecruitmentAutomationPlatform.exceptions.DuplicateJobException;
+import com.example.RecruitmentAutomationPlatform.exceptions.JdGenerateException;
 import com.example.RecruitmentAutomationPlatform.exceptions.NoSuchJobException;
+import com.example.RecruitmentAutomationPlatform.microservice.JdGenerator;
 import com.example.RecruitmentAutomationPlatform.repositories.JobEntityRepository;
 import com.example.RecruitmentAutomationPlatform.services.JobService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JobServiceImpl implements JobService {
 
     private final JobEntityRepository jobRepository;
     private final ModelMapperConfig modelMapper;
+    private final JdGenerator jdGenerator;
 
     @Transactional
     @Override
     public JobResponseDto createJob(JobDto jobRequest) {
+        log.info("[JobService] Looking for job with title {} in company: {}", jobRequest.getTitle(), jobRequest.getCompanyName());
         if(jobRepository.existsByTitleAndCompanyName(jobRequest.getTitle(), jobRequest.getCompanyName())) {
             throw new DuplicateJobException("Job with this title  already exists for company  "+ jobRequest.getCompanyName());
         }
         JobEntity newJobEntity = mapToEntity(jobRequest);
 
+        log.info("[JobService] Calling JD Generation Service for job with Id: {}", newJobEntity.getId());
+//
+//        new JdRequest();
+//        try {
+            JdRequest jdRequest = JdRequest.builder()
+                    .title(newJobEntity.getTitle())
+                    .techStack(newJobEntity.getTechStack())
+//                    .expRange(newJobEntity.getExpRange())
+                    .companyName(newJobEntity.getCompanyName())
+                    .pQualification(newJobEntity.getPQualification())
+                    .mQualification(newJobEntity.getMQualification())
+                    .build();
+            JdResponse jd = jdGenerator.generateJd(jdRequest);
+            newJobEntity.setJd(jd.getJd());
+//        } catch (Exception e) {
+//            newJobEntity.setJd(jobRequest.getJd());
+//            throw new JdGenerateException("Failed to Generate JD for Job");
+//        }
+
+        log.info("[JobService] Saved Job Successfully with Id:{}", newJobEntity.getId());
         final JobEntity savedJobEntity = jobRepository.save(newJobEntity);
+
         return mapToResponse(savedJobEntity);
     }
 
@@ -71,5 +99,7 @@ public class JobServiceImpl implements JobService {
                 jobs -> modelMapper.modelMapper().map(jobs, JobDto.class)
         ).toList();
     }
+
+
 
 }
